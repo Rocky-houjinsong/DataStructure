0c6bfcffd96bf83f68b8f38604f51d3cc795db0b
songhoujin <songhoujin@mediinfo.com.cn>
2023-02-21 20:29:53 +0800

    静态数组 和 动态数组ArrayList,List的初步认识;
    在于 指针是否越界

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,30 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
+                int[] arr = new int[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    arr[i] = i;
+                    Console.Write(arr[i]+" ");
+                }
+
+                Console.WriteLine();
+                //此示例说明 动态数组ArrayList 不会报出 数组越界异常的错误,
+                ArrayList arrayList = new ArrayList(10);
+                for (int i = 0; i < 15; i++)
+                {
+                    arrayList.Add(i);
+                    Console.Write(arrayList[i]+" ");
+                }
+
+                Console.WriteLine();
+                List<int> list = new List<int>(10);
+                for (int i = 0; i < 15; i++)
+                {
+                    list.Add(i);
+                    Console.Write(list[i]+" ");
+                }
+                Console.ReadKey();
         }
     }
 }
